8d30cd60f1723e831cc3a3ea93c4cb29fcbd0822
Habibie1204 <baehakimh@gmail.com>
2023-10-09 13:53:41 +0700

    first commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,10 @@
+setTimeout(function() {
+  $('.about-info-inner').typed({
+    strings: [
+      "<span>Halo, saya Habibie Baehakim</span> Saya adalah seorang mahasiswa yang bersemangat dan penuh dedikasi di semester 5 jurusan Informatika. Dalam perjalanan akademik saya, saya telah menggabungkan pengetahuan teoritis dengan keterampilan praktis untuk memahami konsep-konsep kompleks dalam dunia teknologi informasi. Saya telah aktif terlibat dalam berbagai proyek kelompok yang melibatkan pemrograman, analisis data, desain UI/UX. Kolaborasi dalam tim telah membantu saya memperluas wawasan tentang bagaimana teknologi berfungsi dalam lingkungan yang dinamis. Saya tertarik untuk terus belajar, berkembang, dan berkontribusi dalam dunia teknologi informasi. Dengan kombinasi antara dedikasi, pengetahuan, dan semangat, saya yakin bisa menjadi aset berharga bagi tim atau proyek apa pun di masa depan.</br></br>"
+
+    ],
+    typeSpeed: 7,
+    contentType: 'html'
+  });
+}, 100);
